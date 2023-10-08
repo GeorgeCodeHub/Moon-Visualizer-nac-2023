38f2e14d848db9f4465117d6eee63ef2b5e419db
GeorgeCodeHub <54906099+GeorgeCodeHub@users.noreply.github.com>
2023-10-08 22:30:45 +0300

    add basename for router

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import MoonPage from "./pages/MoonPage";
 
 import "./App.css";
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <LandingPage />
-	},
-	{
-		path: "/moon",
-		element: <MoonPage />
-	}
-]);
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <LandingPage />
+		},
+		{
+			path: "/moon",
+			element: <MoonPage />
+		}
+	],
+	{ basename: import.meta.env.DEV ? "/" : "/Moon-Visualizer-nac-2023/" }
+);
 
 function App() {
 	return <RouterProvider router={router} />;
