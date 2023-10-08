dd3105de5aa8aac0e26dcf2abd72ee63d9b3a3d3
GeorgeCodeHub <54906099+GeorgeCodeHub@users.noreply.github.com>
2023-10-08 22:05:26 +0300

    fix errors on types

--- src/components/Moon.tsx
+++ src/components/Moon.tsx
@@ -19,7 +19,10 @@ const markerSvg = `<svg
 		<path d="M5.75 1C6.16421 1 6.5 1.33579 6.5 1.75V3.6L8.22067 3.25587C9.8712 2.92576 11.5821 3.08284 13.1449 3.70797L13.3486 3.78943C14.9097 4.41389 16.628 4.53051 18.2592 4.1227C19.0165 3.93339 19.75 4.50613 19.75 5.28669V12.6537C19.75 13.298 19.3115 13.8596 18.6864 14.0159L18.472 14.0695C16.7024 14.5119 14.8385 14.3854 13.1449 13.708C11.5821 13.0828 9.8712 12.9258 8.22067 13.2559L6.5 13.6V21.75C6.5 22.1642 6.16421 22.5 5.75 22.5C5.33579 22.5 5 22.1642 5 21.75V1.75C5 1.33579 5.33579 1 5.75 1Z" />
 	</svg>`;
 
-const weightColor = scaleLinear().domain([0, 500]).range(["orange", "darkred"]).clamp(true);
+const weightColor = scaleLinear()
+	.domain([0, 500])
+	.range(["orange", "darkred"] as any)
+	.clamp(true);
 
 const colorInterpolator = (t: any) => `rgba(255,100,50,${Math.sqrt(1 - t)})`;
 
@@ -61,8 +64,8 @@ const Moon: FC<IMoon> = ({ enableRotation = false, dataset = [], moonLandingData
 			hexBinPointsData={data}
 			hexBinPointWeight={(d: any) => d.Depth || 10}
 			hexAltitude={({ sumWeight }) => sumWeight * 0.0005}
-			hexTopColor={(d) => weightColor(d.sumWeight)}
-			hexSideColor={(d) => weightColor(d.sumWeight)}
+			hexTopColor={(d) => weightColor(d.sumWeight) as any}
+			hexSideColor={(d) => weightColor(d.sumWeight) as any}
 			hexLabel={(d: any) => {
 				return `
 				<div style="background-color: #e5e5e5; color: #000; z-index: 5000000; padding: 8px;">
